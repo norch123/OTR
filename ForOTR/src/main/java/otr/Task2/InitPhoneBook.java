de9d30d8f1a38729c446a534912a19c3128c7190
norch123 <norchia@yandex.ru>
2016-12-16 11:24:57 +0400

    Добавил класс User для описания юзера и класс InitPhoneBook для инициализации телефонной книги

---
+++ ForOTR/src/main/java/otr/Task2/InitPhoneBook.java
@@ -0,0 +1,30 @@
+package otr.Task2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class InitPhoneBook {
+	
+	public static Map<User, List<String>> init() {
+		Map<User, List<String>> phoneBook = new HashMap<User, List<String>>();
+    	List<String> phoneNumber1 = new ArrayList<String>();
+    	phoneNumber1.add("+8 800 2000 500");
+    	phoneNumber1.add("+8 800 200 600");
+    	phoneBook.put(new User("Иванов И.И."), phoneNumber1);
+    	
+    	List<String> phoneNumber2 = new ArrayList<String>();
+    	phoneNumber2.add("+8 800 2000 700");
+    	phoneBook.put(new User("Петров П.П."), phoneNumber1);
+    	
+    	List<String> phoneNumber3 = new ArrayList<String>();
+    	phoneNumber3.add("+8 800 2000 800");
+    	phoneNumber3.add("+8 800 2000 900");
+    	phoneNumber3.add("+8 800 2000 000");
+    	phoneBook.put(new User("Сидоров С.С."), phoneNumber1);
+    	
+    	return phoneBook;
+	}
+
+}
